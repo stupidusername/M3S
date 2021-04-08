7b2d6fe155d0837111f3fe0f1086ffb9e9aa4d5c
Nicolas Suarez <nicolas.suarez@naranjax.com>
2021-04-08 11:14:19 -0300

    Fix error in user model

--- models/User.php
+++ models/User.php
@@ -138,7 +138,7 @@ class User extends ActiveRecord implements IdentityInterface {
      * {@inheritdoc}
      */
     public function validateAuthKey($authKey) {
-        return $this->auth_key === $auth_key;
+        return $this->auth_key === $authKey;
     }
 
     /**
